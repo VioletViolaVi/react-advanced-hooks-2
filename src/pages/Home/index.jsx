b7063df9d53794790a951a8b8746ca96e3016e4f
Matt Lilley <m.k.lilley@gmail.com>
2022-08-24 13:44:12 +0100

    Fixes name of Home component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-export default function Profile() {
+export default function Home() {
     const user  = "";
     return (
         <>
